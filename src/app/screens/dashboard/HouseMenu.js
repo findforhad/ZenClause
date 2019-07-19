bacfb5f01d7c4e9b9c9ccbcd103347b1b5c037da
findforhad <forhadhossenbabu@gmail.com>
2019-07-20 02:31:23 +0600

    Version 1.0.x

--- src/app/screens/dashboard/HouseMenu.js
+++ src/app/screens/dashboard/HouseMenu.js
@@ -11,66 +11,213 @@ class HouseMenu extends React.Component {
     let { uid, neighborID } = this.props;
     return (
       <View
-        style={{
-          backgroundColor: "#5e8ac6",
-          padding: 10,
-          borderRadius: 4,
-          borderColor: "#000000",
-          borderWidth: 2,
-          width: "50%",
-          height: 400,
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-around"
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            width: "48%",
-            height: 60,
-            borderColor: "black",
-            borderWidth: 2
-          }}
-          onPress={() => console.log("Invite Method Called")}
-        >
-          <LinearGradient
-            colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
-            style={{
-              alignItems: "center",
-              flex: 1
-            }}
-          >
-            <Text
-              style={[styles.btnText, { textAlign: "center", lineHeight: 60 }]}
-            >
-              INVITE
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
+        style={[
+          {
+            backgroundColor: "#5e8ac6",
+            borderRadius: 4,
+            borderColor: "#000000",
+            borderWidth: 2,
+            width: "50%",
 
-        <TouchableOpacity
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-around",
+            paddingVertical: 18,
+            paddingHorizontal: 10
+          },
+          styles.height("60%")
+        ]}
+      >
+        <View
           style={{
-            width: "48%",
-            height: 60,
-            borderColor: "black",
-            borderWidth: 2
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: 10
           }}
-          onPress={() => console.log("Move Method Called")}
         >
-          <LinearGradient
-            colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+          <TouchableOpacity
             style={{
-              alignItems: "center",
-              flex: 1
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
             }}
+            onPress={() => console.log("Invite Method Called")}
           >
-            <Text
-              style={[styles.btnText, { textAlign: "center", lineHeight: 60 }]}
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
             >
-              MOVE
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                INVITE
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
+            }}
+            onPress={() => console.log("Invite Method Called")}
+          >
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                MOVE
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around"
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
+            }}
+            onPress={() => console.log("Invite Method Called")}
+          >
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                ADD
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
+            }}
+            onPress={() => console.log("Invite Method Called")}
+          >
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                DELETE
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-around"
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
+            }}
+            onPress={() => console.log("Invite Method Called")}
+          >
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                NEXT
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              width: "48%",
+              height: 72,
+              borderColor: "black",
+              borderWidth: 2,
+              borderRadius: 4
+            }}
+            onPress={() => console.log("Invite Method Called")}
+          >
+            <LinearGradient
+              colors={["#005fa6", "#0c8fd6", "#39bcd6"]}
+              style={{
+                alignItems: "center",
+                flex: 1
+              }}
+            >
+              <Text
+                style={[
+                  styles.btnText,
+                  { textAlign: "center", lineHeight: 72 }
+                ]}
+              >
+                CANCEL
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
